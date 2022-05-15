02a297ec66adb5c7e729e5baeae2cd8a75adba3b
Giulian Trabucco <giulian.trabucco@gmail.com>
2022-05-15 14:08:04 -0400

    added sound

--- src/Workout.js
+++ src/Workout.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import 'antd/dist/antd.min.css'
 import './index.css';
 import { InputNumber, Col, Row, Button } from 'antd';
+import useSound from 'use-sound';
+import sound from './sounds/sound1.wav';
 
 function Workout() {
   const [running, setRunning] = useState(false);
@@ -11,6 +13,7 @@ function Workout() {
   const [origTimeBetweenSet, setOrigTimeBetweenSet] = useState(0)
   const [repsTime, setRepsTime] = useState(0)
   const [origRepsTime, setOrigRepsTime] = useState(0)
+  const [play] = useSound(sound);
 
   const onSetsChange = (value) => {
     setSets(value)
@@ -33,6 +36,8 @@ function Workout() {
       setSets(0)
       setTimeBetweenSet(0)
       setOrigTimeBetweenSet(0)
+    } else {
+      play()
     }
 
     setPaused(false)
@@ -57,6 +62,10 @@ function Workout() {
         clearTimeout(token);
       }
     } else if (running && sets > 1 && repsTime === 0 && !paused && timeBetweenSet > 0) {
+      if (timeBetweenSet === origTimeBetweenSet){
+        play()
+      }
+
       const token = setTimeout(() => {
         setTimeBetweenSet(prevTime => prevTime - 1)
       }, 1000)
@@ -65,13 +74,15 @@ function Workout() {
         clearTimeout(token);
       }
     } else if (running && sets > 1 && repsTime === 0 && !paused && timeBetweenSet === 0) {
+      play()
       setSets(prev => prev - 1)
       setRepsTime(origRepsTime)
       setTimeBetweenSet(origTimeBetweenSet)
     } else if (running && sets === 1 && repsTime === 0 && !paused) {
+      play()
       setRunning(false)
     }
-  }, [running, origRepsTime, origTimeBetweenSet, paused, repsTime, sets, timeBetweenSet])
+  }, [running, origRepsTime, origTimeBetweenSet, paused, repsTime, sets, timeBetweenSet,play])
 
   const formComponent = () => {
     return (
@@ -129,25 +140,25 @@ function Workout() {
 
           <Col span={6} offset={4}>
             <Row className="row">
-              <h1>{sets} </h1>           
+              <h1>{sets} </h1>
             </Row>
             <Row className="row">
-              <h1>{timeBetweenSet} </h1>           
+              <h1>{timeBetweenSet} </h1>
             </Row>
             <Row className="row">
-              <h1>{repsTime}</h1>            
+              <h1>{repsTime}</h1>
             </Row>
           </Col>
         </Row >
-        
+
         <Row justify="center" align="bottom" className="row">
           <Col span={12}>
             {paused ? <Button size="large" className="btn-row" block onClick={pauseWorkout}>Resume</Button>
               : <Button size="large" className="btn-row" block onClick={pauseWorkout}>Pause</Button>
-            }          
+            }
           </Col>
           <Col span={12}>
-            <Button size="large" className="btn-row" block onClick={updateWorkout}>End</Button>       
+            <Button size="large" className="btn-row" block onClick={updateWorkout}>End</Button>
           </Col>
         </Row>
       </>
@@ -155,10 +166,10 @@ function Workout() {
   }
 
   return (
-    <div className={ running ? !paused && repsTime > 0 ? "running" : "break" : ""}>
-      { running ? 
-        timerComponent() : 
-        formComponent() }
+    <div className={running ? !paused && repsTime > 0 ? "running" : "break" : ""}>
+      { running ?
+        timerComponent() :
+        formComponent()}
     </div>
   );
 }
